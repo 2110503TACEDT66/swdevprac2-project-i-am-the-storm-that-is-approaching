3c017b06079cfb00ca1b5f0617ba218537877356
Api Phoom <kunsue17@gmail.com>
2024-03-26 00:46:11 +0700

    feat : add new prisma schema

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,6 +6,8 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
+import { AppProviders } from "next-auth/providers";
+import CredentialsProvider from "next-auth/providers/credentials";
 import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "~/env";
@@ -21,6 +23,8 @@ declare module "next-auth" {
   interface Session extends DefaultSession {
     user: DefaultSession["user"] & {
       id: string;
+      firstName: string;
+      lastName: string;
       // ...other properties
       // role: UserRole;
     };
@@ -37,32 +41,101 @@ declare module "next-auth" {
  *
  * @see https://next-auth.js.org/configuration/options
  */
+const providers: AppProviders = [];
+
+providers.push(
+  DiscordProvider({
+    clientId: env.DISCORD_CLIENT_ID,
+    clientSecret: env.DISCORD_CLIENT_SECRET,
+  }),
+  CredentialsProvider({
+    id: "credentials",
+    name: "Credentials",
+    credentials: {
+      email: { label: "email", type: "email", placeholder: "123@email.com" },
+      password: { label: "Password", type: "password" },
+    },
+    async authorize(credentials) {
+      if (!credentials?.email || !credentials?.password) {
+        throw new Error("Email and password are required.");
+      }
+      const { email, password } = credentials;
+
+      const user = await db.user.findUnique({
+        where: { email: email, accounts: { none: {} } },
+      });
+
+      if (!user) {
+        throw new Error(
+          "User not found. Please check your email address or sign up.",
+        );
+      }
+
+      if (!user.password) {
+        throw new Error(
+          "It appears you usually sign in with a social account. Please use your social account to sign in.",
+        );
+      }
+
+      const passwordMatches = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatches) {
+        throw new Error("Incorrect password. Please try again.");
+      }
+
+      await log("login", `User ${user.id} logged in`);
+
+      return user;
+    },
+  }),
+);
+
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  providers: providers,
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
-    session: ({ session, user }) => ({
+    session: ({ session, token }) => ({
       ...session,
       user: {
         ...session.user,
-        id: user.id,
+        id: token.sub,
+        firstName: token.firstName,
+        lastName: token.lastName,
+        name: token.name,
       },
     }),
+    async jwt({ token, trigger }) {
+      if (trigger === "update" || trigger === "signIn") {
+        const userData = await db.user.findUnique({
+          where: {
+            id: token.sub,
+          },
+          select: {
+            name: true,
+            profile: {
+              select: {
+                firstName: true,
+                lastName: true,
+              },
+            },
+          },
+        });
+        if (userData) {
+          token.name = userData.name;
+          token.firstName = userData.profile.firstName;
+          token.lastName = userData.profile.lastName;
+        }
+      }
+      return token;
+    },
   },
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
+  pages: {
+    signIn: "/",
+  },
+  secret: env.NEXTAUTH_SECRET,
 };
 
 /**
